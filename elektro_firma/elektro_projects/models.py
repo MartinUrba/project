e714e440e2c955a7d694374fa6b54a11eabe8a48
MartinUrba <Murban78@email.cz>
2024-11-30 20:02:28 +0100

    nová aktualizace

--- elektro_firma/elektro_projects/models.py
+++ elektro_firma/elektro_projects/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.urls import reverse
 from people.models import Person
 
+
 class Project(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     customer = models.CharField(max_length=255)
-    assigned_to = models.ManyToManyField(Person, related_name='projects')  # Přidání ManyToManyField
+    assigned_to = models.ManyToManyField(Person, related_name='projects')  # Propojení s Person
     image = models.ImageField(upload_to='projects/images/', blank=True, null=True)
 
     def __str__(self):
